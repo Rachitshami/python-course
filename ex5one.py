6e09e5ae8b97f6e57eea3f9fd30887f1e3b7b239
rachitshami <rachitshami03@gmail.com>
2018-10-15 21:42:32 +0530

    ex4 to ex11

---
+++ ex5one.py
@@ -0,0 +1,19 @@
+name = "Rachit"
+age = 28
+height = 74 # inches
+height_cm = height * 2.54
+weight = 180 # lbs
+weight_Kg = round(weight / 2.205)
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {height_cm} cms tall.")
+print(f"He's {weight} pounds heavy.")
+print(f"He's {weight_Kg} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+total = age + height + weight
+print(f"If I add {age},{height},and {weight} I get {total}.")
